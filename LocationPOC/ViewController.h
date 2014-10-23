c71759ed7e348246aaef9c4709406be9b3ad0bb5
Eric Internicola <einternicola@silverchalice.com>
2014-10-22 20:50:09 -0600

    Initial Commit

---
+++ LocationPOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationPOC
+//
+//  Created by Eric Internicola on 10/22/14.
+//  Copyright (c) 2014 Brothers Magoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
